58a9ec5f64107420b7c153f5ca26045d28b372c3
Antyperspirant36 <167078662+Antyperspirant36@users.noreply.github.com>
2024-11-02 13:01:59 +0100

    Poddaje sie z fetchem

--- airquality/scripts/airquality.js
+++ airquality/scripts/airquality.js
@@ -1,13 +1,2 @@
-fetch("", {
-    method: 'GET', // or 'POST' depending on what you're doing
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Handle the data received from the backend
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+//Poddaję się - Krzycho
+//Twoja kolej Karol
